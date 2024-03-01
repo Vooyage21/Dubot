5ef9501b4f018a4e21f7df5e8dce90befb9f6bc4
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-01 18:08:12 +0700

    Update and rename Admin.py to admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,12 +1,6 @@
-# Ayra - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/Ayra/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 """
-◈ Perintah Tersedia
+⍚ Perintah Tersedia
 
 • `{i} promote` <balas ke pengguna/userid/username>
 
