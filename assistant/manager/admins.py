ae0e9766b73705824edb862e23808d343bdf417d
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-05 13:12:24 +0700

    Update admins.py

--- assistant/manager/admins.py
+++ assistant/manager/admins.py
@@ -1,20 +1,14 @@
-# Ayra - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/Ayra/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 import re
 
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 
-from Kazu import _kazu_cache
+from dante import _dante_cache
 
 from . import *
 
 
-@ayra_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
+@dante_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
 async def dowj(e):
     replied = await e.get_reply_message()
     if replied:
