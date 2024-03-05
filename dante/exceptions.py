d43ad0bba1732fac6684b4bb9e15db385f4e373d
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-05 13:23:22 +0700

    Update exceptions.py

--- dante/exceptions.py
+++ dante/exceptions.py
@@ -1,12 +1,6 @@
-# Ayra - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/Ayra/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 """
-Exceptions which can be raised by Kazu Ubot Itself.
+Exceptions which can be raised by dante Ubot Itself.
 """
 
 
