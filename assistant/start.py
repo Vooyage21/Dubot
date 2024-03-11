6517d0833d66baa51349a55934c72546897ea7de
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-12 02:11:57 +0700

    update assistant/start.py

--- assistant/start.py
+++ assistant/start.py
@@ -98,7 +98,7 @@ async def ayra(event):
             )
     if event.sender_id not in SUDO_M.fullsudos:
         ok = ""
-        me = inline_mention(kazu_bot.me)
+        me = inline_mention(dante_bot.me)
         mention = inline_mention(event.sender)
         if args and args != "set":
             await get_stored_file(event, args)
